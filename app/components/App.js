aac08ffa77829e10cbf347c3c24d9147dfa2cbc9
chlde <411150056@qq.com>
2016-12-27 23:08:46 +0800

    add app/components

---
+++ app/components/App.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Administrator on 2016/12/27.
+ */
+import React from 'react';
+import {RouteHandler} from 'react-router';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <RouteHandler />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ app/components/Home.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2016/12/27.
+ */
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div className='alert alert-info'>
+                Hello from Home Component
+            </div>
+        );
+    }
+}
+
+export default Home;
