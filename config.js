6518cba573198ee7026f16264d654faa0520916c
chenhaolong <411150056@qq.com>
2017-02-15 17:01:09 +0800

    delete config

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
  * Copyright (C) 2017 Baidu, Inc. All Rights Reserved.
  */
 module.exports = {
-  database: process.env.MONGO_URI || 'cp01-rdqa-dev352.cp01.baidu.com:8110/blogboost'
+  database: process.env.MONGO_URI || 'cp01-rdqa-dev352.cp01.google.com:8110/blogboost'
 };
