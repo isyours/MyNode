eaa2af1f9bef72ff9047e82ae393cf4784e25768
Administrator <411150056@qq.com>
2017-05-15 22:32:45 +0800

    update shell

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+gulp build
 ps -ef | grep "/home/web/myblog/MyNode/node_modules/babel/lib/_babel-node" | grep -v grep | awk '{print $2}' | xargs kill -9
 mv running.log running.log.$(date +%Y%m%d%H)
 /usr/bin/nodejs /home/web/myblog/MyNode/node_modules/babel/lib/_babel-node server.js >> running.log&
