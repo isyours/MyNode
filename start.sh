1402ce57a2e14e2a2ff36c83102a0790043a48ab
Administrator <411150056@qq.com>
2017-05-17 23:06:25 +0800

    update start shell

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-forever start -a --pidFile $BLOG_HOME/pid.file -l $LOG/forever.log -o $LOG/out.log -e $LOG/err.log $BLOG_HOME/server.js
+forever start -a --pidFile $BLOG_HOME/pid.file -l $LOG/forever.log -o $LOG/out.log -e $LOG/err.log server.js
