f54a2da90be2ed704db8704596838e121826186b
玉鲲 <haolong.chl@alibaba-inc.com>
2017-07-13 22:55:21 +0800

    update seo strategy, add page description in meta area.

--- server/controller/seo.js
+++ server/controller/seo.js
@@ -13,19 +13,17 @@ exports.renderWithArticlesBrafeList = async(function* (req, res, next) {
         global.logger.info('Render Seo Blog list, Error info', err);
         if (err) return next(err);
         global.logger.info('Render Seo Blog list success');
-        let pageName = "Gkwen 玉鲲 内容与摘要";
+        let pageInfo = generatePageInfo("玉鲲的博客", "玉鲲技术博客，专注于web技术与数据分析");
         let list = [];
         if (blogList) {
             blogList.map(function (item) {
-                list.push({
-                    link: url + 'blog/' + item.blogTitle,
-                    content: item.blogBrief
-                });
+                let link = url + 'blog/' + item.blogTitle;
+                list.push(generateArticle(link, item.blogBrief, item.blogContent, "", item.blogTitle));
             });
         }
 
         return res.send(swig.renderFile('views/index.html', {
-            pagename: pageName,
+            page: pageInfo,
             articles: list
         }));
     });
@@ -36,31 +34,50 @@ exports.renderWithArticleAnchor = async(function* (req, res, next) {
 
     Blog.findOne({blogTitle: blogTitle}, function(err,obj) {
         if (err) return next(err);
-        let htmlContent = obj.blogContent;
+        // let htmlContent = obj.blogContent;
         let renderList = [];
-        let pageName = obj.blogName;
+        let pageInfo = generatePageInfo(obj.blogName, obj.blogBrief);;
 
-        let container = $("<div></div>");
-        container.append($(htmlContent));
-        let attrs = container.find("h1, h2, h3, h4, h5, h6");
-        if (!attrs || attrs.length === 0) {
-            return res.send(swig.renderFile('views/index.html', {
-                pagename: pageName,
-                articles: renderList
-            }))
-        }
-
-        attrs.map(function(index, item) {
-            let jItem = $(item);
-            renderList.push({
-                link: url + 'blog/' + blogTitle + "#" + jItem.attr("id"),
-                content: jItem.text()
-            });
-        });
+        // let container = $("<div></div>");
+        // container.append($(htmlContent));
+        // let attrs = container.find("h1, h2, h3, h4, h5, h6");
+        // if (!attrs || attrs.length === 0) {
+        //     return res.send(swig.renderFile('views/index.html', {
+        //         page: pageInfo,
+        //         articles: renderList
+        //     }))
+        // }
+        //
+        // attrs.map(function(index, item) {
+        //     let jItem = $(item);
+        //     renderList.push(generateArtical(l){
+        //         link: url + 'blog/' + blogTitle + "#" + jItem.attr("id"),
+        //         content: jItem.text()
+        //     });
+        // });
+        let link = url + 'blog/' + obj.blogTitle;
+        renderList.push(generateArticle(link, obj.blogBrief, obj.blogContent, obj.blogContent, obj.blogTitle));
 
         return res.send(swig.renderFile('views/index.html', {
-            pagename: pageName,
+            page: pageInfo,
             articles: renderList
         }))
     });
 });
+
+function generatePageInfo(pageName, pageDescription) {
+    return {
+        name: pageName,
+        description: pageDescription
+    }
+}
+
+function generateArticle(link, brief, title, content, updateTime) {
+    return {
+        link: link,
+        brief: brief,
+        title: title,
+        content: content,
+        updateTime: updateTime
+    }
+}
